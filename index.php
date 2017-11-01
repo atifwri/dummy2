be370113aca6b9af45d74d55ffa75f13d3e79ff1
Atif <atif.wri121@webreinvent.com>
2017-11-01 11:26:19 +0530

    First commit after adding files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require 'vendor/autoload.php';
+
+$client = new GuzzleHttp\Client();
+$res = $client->request('GET', 'http://webreinvent.com/', [
+    'auth' => ['user', 'pass']
+]);
+echo $res->getStatusCode();
+// "200"
+echo $res->getHeader('content-type');
+// 'application/json; charset=utf8'
+echo $res->getBody();
+// {"type":"User"...'
+
+?>
